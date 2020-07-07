931794667f18a838099d97525460c2c8c240610c
paulus <paulusyeung@gmail.com>
2020-07-07 11:14:48 -0700

    Akaunting zh-TW Invoice ＝ 發票

--- resources/lang/zh-TW/invoices.php
+++ resources/lang/zh-TW/invoices.php
@@ -2,8 +2,8 @@
 
 return [
 
-    'invoice_number'        => '訂單號碼',
-    'invoice_date'          => '訂單日期',
+    'invoice_number'        => '發票號碼',
+    'invoice_date'          => '發票日期',
     'total_price'           => '總價',
     'due_date'              => '到期日',
     'order_number'          => '訂單編號',
